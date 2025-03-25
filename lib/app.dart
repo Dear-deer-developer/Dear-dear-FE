9c894911af82f3c8774e1623370772b738c0e012
Obebe <shkim000905@naver.com>
2025-03-25 17:27:47 +0900

    초기 설정

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:dear_deer_demo/view/home.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dear.deer Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
