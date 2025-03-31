285d522d232ed3356830829367643fdb6ab989ef
Obebe <shkim000905@naver.com>
2025-03-31 19:55:55 +0900

    캘린더 만들기 연습 시작

--- lib/view/login/login_main.dart
+++ lib/view/login/login_main.dart
@@ -19,13 +19,45 @@ class LoginMain extends StatelessWidget {
           Center(
             child: Padding(
               padding: EdgeInsets.only(bottom: 80.h),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(10.r)),
-                width: 300.w,
-                height: 45.h,
-                child: Center(child: Text("로그인 테스트")),
+              child: Column(
+                children: [
+                  // 캘린더 연습 - 성현
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10.r)),
+                    width: 300.w,
+                    height: 45.h,
+                    child: const Center(child: Text("캘린더 연습 성현")),
+                  ),
+                  // 캘린더 연습 - 채림
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10.r)),
+                    width: 300.w,
+                    height: 45.h,
+                    child: const Center(child: Text("캘린더 연습 채림")),
+                  ),
+                  // 캘린더 연습 - 성은
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.purple,
+                        borderRadius: BorderRadius.circular(10.r)),
+                    width: 300.w,
+                    height: 45.h,
+                    child: const Center(child: Text("캘린더 연습 성은")),
+                  ),
+                  // 임시 로그인 버튼
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(10.r)),
+                    width: 300.w,
+                    height: 45.h,
+                    child: const Center(child: Text("로그인 테스트")),
+                  ),
+                ],
               ),
             ),
           )
