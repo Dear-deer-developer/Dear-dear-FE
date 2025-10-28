510ccd43c60fc348efbf8075ad37e158f4a9312b
Obebe <shkim000905@naver.com>
2025-10-29 00:46:42 +0900

    feat/contents develop

---
+++ lib/view/contents/contents_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ContentsMain extends StatelessWidget {
+  const ContentsMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
